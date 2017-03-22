5fbf60d9738b1bf7177fe815403d81487465e759
Daniel Chempolayil Abraham <daniel.cabraham@in.bosch.com>
2017-03-22 14:48:17 +0530

    New DNL folder addition

--- rpmbuild/SOURCES/v850-fw/MausBus.h
+++
@@ -1,5 +0,0 @@
-#ifndef _MausBus
-#define _MausBus
-bool MausSend (RS232Typ *pPort, unsigned char cCommand, unsigned char *pcDataBuffer, unsigned char cLength);
-bool MausReceive (RS232Typ *pPort, unsigned char cCommand, unsigned char *pucReceiveBuffer, unsigned char *pucReceiveBufferLength, unsigned long ulTrys, unsigned int uiMode);
-#endif
